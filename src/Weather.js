1ca132c41a7c426873f25cf2a16d97d78c97dcc1
T-queen <142670502+T-queen@users.noreply.github.com>
2023-12-11 23:03:43 +0100

    Added axios integration so it displays the weather

---
+++ src/Weather.js
@@ -0,0 +1,27 @@
+import React, {useEffect} from "react";
+import axios from 'axios';
+import { Audio } from 'react-loader-spinner'
+
+export default function Weather(props) {
+    useEffect(() => {
+        function handleResponse(response) {
+            alert(`The weather in ${response.data.name} is ${response.data.main.temp}°C`);
+        }
+        let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=2b1ee82be7aebff078121568f54f3a4c&units=metric`;
+
+        axios.get(apiUrl)
+        .then(handleResponse);
+}, []); // Add an empty dependency array to fetch data only once when the component is mounted
+
+  return (
+    <Audio
+  height="80"
+  width="80"
+  radius="9"
+  color="green"
+  ariaLabel="loading"
+  wrapperStyle
+  wrapperClass
+/>
+  );
+}
